092a69e01b3f08d620f6ab924a1914bb2a9eff14
Quill jou <quillzhou@gmail.com>
2016-08-19 21:29:13 +0800

    add css 3d demo

---
+++ demo/css-tricks/3D/3D-Carousel/main.js
@@ -0,0 +1,65 @@
+window.onload = function() {
+	var btns = document.getElementsByTagName('a');
+	var carousel = document.getElementById('carousel');
+	var defaultDuration = getAttr('#carousel','animationDuration');
+
+	var degree = 0;
+	btns[0].onclick = prev;
+	function prev(){
+		degree += 40;
+		carousel.style.transform = 'rotateY(' + degree + 'deg) translateZ(288px)';
+		console.log(degree);
+	}
+
+	btns[2].onclick = stop;
+
+	function stop(){
+		carousel.classList.toggle('stop');
+		carousel.classList.toggle('shake');
+		this.innerText = this.innerText == 'Start' ? 'Stop' : 'Start';
+	}
+
+
+    btns[3].onclick = function(){    /*faster*/
+        var curDuration = getAttr('#carousel','animationDuration');
+        if ( parseInt(curDuration) > 4) {
+            var nextDuration = parseInt(curDuration) - 2;
+            nextDurationStr = nextDuration + 's';
+            setAttr('#carousel','animationDuration',nextDurationStr);
+        }
+    }
+
+	btns[4].onclick = function(){    /*Slower*/
+		var curDuration = getAttr('#carousel','animationDuration');
+		if ( parseInt(curDuration) < 40) {
+			var nextDuration = parseInt(curDuration) + 2;
+			nextDurationStr = nextDuration + 's';
+			setAttr('#carousel','animationDuration',nextDurationStr);
+		}
+	}
+
+	btns[5].onclick = function(){    /*Reload*/
+			setAttr('#carousel','animationDuration',defaultDuration);
+	}
+
+
+
+
+    function getAttr(selector,attr){
+        var classes = document.styleSheets[0].cssRules || document.styleSheets[0].rules
+        for (var i = 0, len = classes.length; i < len; i++ ) {
+            if(classes[i].selectorText == selector){
+                return classes[i].style[attr];
+            }
+        }
+    }
+
+    function setAttr(selector,attr,value){
+        var classes = document.styleSheets[0].cssRules || document.styleSheets[0].rules
+        for (var i = 0, len = classes.length; i < len; i++ ) {
+            if(classes[i].selectorText == selector){
+                classes[i].style[attr] = value;
+            }
+        }
+    }
+}
